6f7e8b82429d75440731175179298d27193d4b53
Binary44 <binarycoder.com@gmail.com>
2022-09-01 12:24:20 +0100

    style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import vs from "../assets/js.jpeg";
+import "./Card.css";
 
 const Card = () => {
   return (
-    <div>
-        <h1>Card</h1>
+    <div className="card">
+      {/* <h1>Updates</h1> */}
+      <div className="gal">
+        <img className="cover" src={vs} alt="" />
+        <div className="dec">
+          <h1>Defi Pay</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eius,
+            veritatis dolor neque consequatur libero officia fugiat ea ipsum
+            perspiciatis explicabo voluptatibus eligendi reiciendis, iure
+            recusandae, necessitatibus aut aspernatur sapiente!
+          </p>
+        </div>
+      </div>
+      <div className="gal">
+        <img className="cover" src={vs} alt="" />
+        <div className="dec">
+          <h1>Defi Pay</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eius,
+            veritatis dolor neque consequatur libero officia fugiat ea ipsum
+            perspiciatis explicabo voluptatibus eligendi reiciendis, iure
+            recusandae, necessitatibus aut aspernatur sapiente!
+          </p>
+        </div>
+      </div>
+      <div className="gal">
+        <img className="cover" src={vs} alt="" />
+        <div className="dec">
+          <h1>Defi Pay</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eius,
+            veritatis dolor neque consequatur libero officia fugiat ea ipsum
+            perspiciatis explicabo voluptatibus eligendi reiciendis, iure
+            recusandae, necessitatibus aut aspernatur sapiente!
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
